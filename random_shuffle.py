75e9392fb08d89c322fd9739709a2f33a9c39a10
JKChai <jk.chai@okstate.edu>
2023-05-06 23:40:17 -0500

    Update script

--- random_shuffle.py
+++ random_shuffle.py
@@ -4,15 +4,42 @@ import os
 import shutil
 import random
 
-dirpath = '/mnt/c/Users/chai8/repos/PY-Shuffle/archive/'
-destDirectory = '/mnt/c/Users/chai8/repos/PY-Shuffle/brain_data'
+dirpath = 'C:/Users/chai8/source/repos/PY-Shuffle/archive/'
+destDir = 'C:/Users/chai8/source/repos/PY-Shuffle/brain_data'
 
-print(os.listdir(dirpath + 'Training/'))
+train_dir = dirpath + 'Training/'
+test_dir  = dirpath + 'Testing/'
+# ['glioma', 'meningioma', 'notumor', 'pituitary'] ## 530,530,1590,530
+# print(len(os.listdir(notumortrainpath))) ## 1595 / 3 = 531
+
+train_counts = []
+train_labels = []
+
+for label in os.listdir(train_dir):
+    train_labels.append(label)
+    train_counts.append(len(os.listdir(train_dir+label)))
+
+print(train_labels)
+print(train_counts)
 
 
+# print(len([name for name in os.listdir(notumortrainpath) if os.path.isfile(name)]))
 
-# filenames = random.sample(os.listdir(dirpath), 100)
 
-# for fname in filenames:
-#     srcpath = os.path.join(dirpath, fname)
-#     shutil.copyfile(srcpath, destDirectory)
+x_labels = ['glioma', 'meningioma', 'pituitary']
+
+# for x_label in x_labels:
+#     filenames = random.sample(os.listdir(train_dir + x_label), 530)
+
+#     for fname in filenames:
+#         srcpath = os.path.join(train_dir + x_label, fname)
+#         # print(srcpath)
+#         shutil.copy(srcpath, destDir + 'train/tumor')
+
+## testing data
+for x_label in x_labels:
+    filenames = random.sample(os.listdir(test_dir + x_label), 135)
+
+    for fname in filenames:
+        srcpath = os.path.join(test_dir + x_label, fname)
+        shutil.copy(srcpath, destDir + '/test/tumor')
